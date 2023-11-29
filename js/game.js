9bdac706794c829a4ceee405aaa79442c00c7418
Veronika Ulybina <veronica.ulybina@yandex.ru>
2023-11-29 14:24:44 +0500

    modified game.js

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const game = (language = 'ru') => {
+  const game = (language) => {
     let lang;
     try {
       lang = Intl.DisplayNames(['en'], {
